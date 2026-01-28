6e65e0e4ea9e8f7b7940a4b3cab2f9b02385f263
Bryan Pineda <bryan@friendlypartyrental.com>
2026-01-27 23:00:34 -0500

    Redirect root URL to /freestyle/

--- config/urls.py
+++ config/urls.py
@@ -1,26 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-
-    # TV page
-    path("freestyle/", include("freestyle.urls")),
-
-    # API endpoints used by the TV
-    path("api/", include("freestyle.api_urls")),
-]
-
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
-
-def root(request):
-    return redirect("/freestyle/")
-
-urlpatterns = [
-    path("", root),
+    path("", RedirectView.as_view(url="/freestyle/", permanent=False)),
     path("admin/", admin.site.urls),
     path("freestyle/", include("freestyle.urls")),
-    path("api/", include("freestyle.api_urls")),
+    path("api/", include("freestyle.api.urls")),
 ]
